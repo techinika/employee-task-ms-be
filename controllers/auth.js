d1d2d2196d67daf48664a250e29523f5f46dc594
Aime Bruce <iraguhaaimebruce@gmail.com>
2023-04-14 01:57:59 +0200

    end point for creating a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,95 @@ const login = (req, res) => {
         }
     })
 }
+
+const register = async (req, res) => {
+    const {firstname, lastname, username, email, password, department} = await req.body;
+    const hashed_password = await bcrypt.hash(password, 8)
+    console.log(hashed_password)
+    const email_exists = await conn.query(`SELECT email FROM users WHERE email= '${email}'`, (err,result) => {
+        if(result){
+            console.log(result[0].email);
+        }
+        return(result[0].email)
+    })
+    const username_exists = await conn.query(`SELECT username FROM users WHERE username= '${username}'`, (err,result) => {
+        if(result){
+            console.log(result[0].username);
+        }
+        return (result[0].username)
+    })
+    const {dep_exists} = await conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
+        if(err){
+            console.log(err)
+        }
+        if(result[0].id < 1){
+            res.status(400).json({
+                error: "No such department found"
+            })
+            return
+        }else{
+            console.log(result[0].id)
+            return result[0].id
+        }
+        
+    })
+    // const dep_id = department_id[0];
+    // const dep_id = dep_exists
+    console.log(dep_exists)
+    const new_user = {
+        firstname: firstname, 
+        lastname: lastname, 
+        username: username, 
+        email: email, 
+        password: hashed_password, 
+        dep_id: department
+    }
+    // console.log()
+    if(!firstname || !lastname || !username || !email || !password || !department){
+        res.status(400).json({error: "All fields must be filled"})
+    }else if(email_exists > 0){
+        res.status(400).json({error: "Email already exists"})
+    }else if(username_exists > 0){
+        res.status(400).json({error: "Username already exists"})
+    }else{
+        conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
+            if(err){
+                console.log(err)
+            }else if(result < 1){
+                console.log("User not found");
+                res.json({
+                    message: "Department not found"
+                })
+            }else{
+                // let dep_id = result[0]
+                const dep_id = result[0].id
+                console.log(dep_id);
+                let sql = `INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}')`
+                conn.query(sql, (err, result) => {
+                    if(err){
+                        console.log(err);
+                        // console.log(req.body);
+                    }else{
+                        res.status(200).json({
+                            message: "User registered successfully"
+                        })
+                    }
+                })
+            }
+        })
+        // conn.query(`INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}'`, (err, result) => {
+        //     if(err){
+        //         console.log(err)
+        //     }else{
+        //         res.status(200).json({
+        //             message: "User registered successfully"
+        //         })
+        //     }
+        // })
+        // res.status(200).json({message: "User registered dsuccessfully"})
+    }
+}
 module.exports = {
-    login
+    login,
+    register
 }
--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,9 +1,10 @@
 // Getting libraries and external functions or variables
 const express = require("express");
-const { login } = require("../controllers/auth");
+const { login, register } = require("../controllers/auth");
 const router = express.Router();
 
 // Defining auth route
-router.route("/").post(login);
+router.route("/login").post(login);
+router.route("/register").post(register);
 
 module.exports = router;
