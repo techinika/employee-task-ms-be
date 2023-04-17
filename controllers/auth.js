e24cac9b84cf5155eb3b409f09c5337fa991a00b
Cedric0852 <mugishac777@gmail.com>
2023-04-17 20:19:58 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,108 +67,152 @@ const login = (req, res) => {
             });
         }
     })
-};
-
-  // Compare the user input password to the hashed password
-  
-
-//     let sql = `SELECT password FROM users WHERE username= '${username}'`;
-//     conn.query(sql, async (err, result) => {
-//         let returned = await result[0];
-//         console.log(returned)
-//         // const match = await bcrypt.compare(password, returned.password)
-//         // const pass = "12345"
-//         // const hashedpass = await bcrypt.hash(pass, 8)
-//         // if(await bcrypt.compare("12345", hashedpass)){
-//         //     console.log("Valid password")
-//         // }else{
-//         //     console.log("Invalid password")
-//         // }
-//         if(err){
-//             res.status(200).json({
-//                 error: "User not found"
-//             })
-//         }else if(!username || !password){
-//             res.status(400).json({
-//                 error: "All fields must be filled"
-//             })
-//         }else if(!returned){
-//             console.log("No such user found in database");
-//             res.status(200).json({
-//                 error: "No such user found in database"
-//             });
-//         }else if(await bcrypt.compare(password, returned.password)){
-//             console.log("Welcome", returned.username)
-//             const access_token = jwt.sign({
-//                 user: {
-//                     firstname: returned.firstname,
-//                     lastname: returned.lastname,
-//                     username: returned.username,
-//                     email: returned.email,
-//                     dep_id: returned.dep_id
-//                 },
-//             }, process.env.ACCESS_TOKEN_SECRET,
-//             {
-//                 expiresIn: "20m"
-//             }
-//             )
-//             res.status(200).json({ access_token })
-//         }else{
-//             res.status(200).json({
-//                 error: "Invalid passord"
-//             })
-//         }
-//     })
+}
 
 //END OF NEW LOGIN AND START OF REGISTER
 const register = async (req, res) => {
     const {firstname, lastname, username, email, password, department} = await req.body;
     const hashed_password = await bcrypt.hash(password, 8)
-
+    console.log(hashed_password)
     console.log(firstname, lastname, username, email, password, department)
-    conn.query(`SELECT email, username FROM users WHERE username='${username}'`, async (err, result) => {
-        const returned = await result[0];
-        // console.log(returned.email)
+    conn.query(`SELECT email FROM users WHERE email= '${email}'`, (err, result) => {
+        const returned = result;
         if(err){
             console.log(err);
         }else if(!firstname || !lastname || !username || !email || !password || !department){
-            res.status(200).json({
+            res.status(400).json({
                 error: "All fields must be filled"
             })
-        }else if(!returned){
-            conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
-            if(err){
-                console.log(err)
-            }else if(result){
-                console.log(result[0].id)
-                const dep_id = result[0].id
-                conn.query(`INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}')`, (err, result) => {
-                    if(err){
-                        console.log(err)
-                    }else if(result){
-                        res.status(200).json({
-                            message: "User registered"
-                        })
-                        console.log(result)
-                    }
-                })
-            }else{
-                    res.status(400).json({
-                        error: "No such department in database"
-                    })
-                }
-                // const dep_id = result[0].id;
-                // module.exports = dep_id;
-                // return(result[0].id)
-            })
-        }else if(returned.username.length > 0){
+        }else if(returned.length > 0){
             res.status(400).json({
-                message: "Username already in use"
-            });
-            console.log(returned.username)
+                error: "Email already in use"
+            })
+            process.exit()
         }
     })
 
+    conn.query(`SELECT username FROM users WHERE username= '${username}'`, (err, result) => {
+        if(err){
+            console.log(err)
+        }else if(result.length > 0){
+            res.status(400).json({
+                message: "Username already in use"
+            });
+            process.exit()
+        }
+    });
+
+    conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
+        if(err){
+            console.log(err)
+        }else if(result){
+            console.log(result[0].id)
+            const dep_id = result[0].id
+            conn.query(`INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}')`, (err, result) => {
+                if(err){
+                    console.log(err)
+                }else{
+                    res.status(200).json({
+                        message: "User registered"
+                    })
+                }
+            })
+        }else{
+            res.status(400).json({
+                error: "No such department in database"
+            })
+        }
+        // const dep_id = result[0].id;
+        // module.exports = dep_id;
+        // return(result[0].id)
+    })
+    
+    // conn.query(`INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}')`, (err, result) => {
+    //     if(err){
+    //         console.log(err)
+    //     }else{
+    //         res.status(200).json({
+    //             message: "User registered"
+    //         })
+    //     }
+    // })
+
+    // const username_exists = await conn.query(`SELECT username FROM users WHERE username= '${username}'`, (err,result) => {
+    //     if(result){
+    //         console.log(result[0].username);
+    //     }
+    //     return (result[0].username)
+    // })
+    // const {dep_exists} = await conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
+    //     if(err){
+    //         console.log(err)
+    //     }
+    //     if(result[0].id < 1){
+    //         res.status(400).json({
+    //             error: "No such department found"
+    //         })
+    //         return
+    //     }else{
+    //         console.log(result[0].id)
+    //         return result[0].id
+    //     }
+        
+    // })
+    // // const dep_id = department_id[0];
+    // // const dep_id = dep_exists
+    // console.log(dep_exists)
+    // const new_user = {
+    //     firstname: firstname, 
+    //     lastname: lastname, 
+    //     username: username, 
+    //     email: email, 
+    //     password: hashed_password, 
+    //     dep_id: department
+    // }
+    // // console.log()
+    // if(!firstname || !lastname || !username || !email || !password || !department){
+    //     res.status(400).json({error: "All fields must be filled"})
+    // }else if(email_exists > 0){
+    //     res.status(400).json({error: "Email already exists"})
+    // }else if(username_exists > 0){
+    //     res.status(400).json({error: "Username already exists"})
+    // }else{
+    //     conn.query(`SELECT id FROM department WHERE name= '${department}'`, (err, result) => {
+    //         if(err){
+    //             console.log(err)
+    //         }else if(result < 1){
+    //             console.log("User not found");
+    //             res.json({
+    //                 message: "Department not found"
+    //             })
+    //         }else{
+    //             // let dep_id = result[0]
+    //             const dep_id = result[0].id
+    //             console.log(dep_id);
+    //             let sql = `INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}')`
+    //             conn.query(sql, (err, result) => {
+    //                 if(err){
+    //                     console.log(err);
+    //                     // console.log(req.body);
+    //                 }else{
+    //                     res.status(200).json({
+    //                         message: "User registered successfully"
+    //                     })
+    //                 }
+    //             })
+    //         }
+    //     })
+        // conn.query(`INSERT INTO users (firstname, lastname, username, email, password, dep_id) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hashed_password}', '${dep_id}'`, (err, result) => {
+        //     if(err){
+        //         console.log(err)
+        //     }else{
+        //         res.status(200).json({
+        //             message: "User registered successfully"
+        //         })
+        //     }
+        // })
+        // res.status(200).json({message: "User registered dsuccessfully"})
+    // }
 }
 module.exports = {
     login,
