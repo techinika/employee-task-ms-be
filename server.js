924d13cf88ccd4e83737f7a9e64abc824eb90ad4
Cedric0852 <mugishac777@gmail.com>
2023-04-17 20:19:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const path = require("path");
 const bodyParser = require("body-parser");
+const { Console } = require("console");
 
 // Using a body parser 
 app.use(bodyParser.urlencoded({ extended : true}))
 app.use(bodyParser.json())
 app.use(cors())
 
-const dotenv = require("dotenv");
-const err_handler = require("./middleware/err_handler");
+const dotenv = require("dotenv")
 dotenv.config()
 // console.log(process.env)
 
@@ -24,7 +25,6 @@ app.use("/auth", require("./routes/auth_route"));
 app.use("/emp", require("./routes/employee"))
 app.use("/admin", require("./routes/admin"))
 
-// app.use(err_handler)
 
 //Setting up a server
 const port = process.env.PORT;
