27ce9a31c3ab5f4a6a57213d004535d0b8215496
Cedric0852 <mugishac777@gmail.com>
2023-04-17 18:09:32 +0200

    Fix auth
    Fixing Authentication, database error ,adding password matching function ,avoiding hash password leak to the client

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,47 +18,58 @@ conn.connect((err) => {
 })
 
 // Retrieving Data from database
+// 
+//NEW LOGIN AND CHECK PASSWORD MATCH USING BCRYPTJS
 const login = (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     let sql = `SELECT * FROM users WHERE username= '${username}'`;
     conn.query(sql, async (err, result) => {
-        let returned = await result[0];
-        console.log(returned)
-        if(err){
+        if (err) {
             res.status(200).json({
                 error: "User not found"
-            })
-        }else if(!username || !password){
+            });
+        } else if (!username || !password) {
             res.status(400).json({
                 error: "All fields must be filled"
-            })
-        }else if(returned.username < 1){
+            });
+        } else if (result.length === 0) {
             console.log("No such user found in database");
             res.status(200).json({
                 error: "No such user found in database"
             });
-        }else if(returned.username && bcrypt.compare(password, returned.password)){
-            console.log("Welcome", returned.username)
-            const access_token = jwt.sign({
-                user: {
-                    firstname: returned.firstname,
-                    lastname: returned.lastname,
-                    username: returned.username,
-                    email: returned.email,
-                    dep_id: returned.dep_id
-                },
-            }, process.env.ACCESS_TOKEN_SECRET,
-            {
-                expiresIn: "20m"
-            }
-            )
-            res.status(200).json({ access_token })
+        } else {
+            const { id, firstname, lastname, username, email, password: hashedPassword, dep_id } = result[0];
+            bcrypt.compare(password, hashedPassword, function(err, result) {
+                if(result === true) {
+                    console.log("Welcome", username);
+                    const access_token = jwt.sign({
+                        user: {
+                            id,
+                            firstname,
+                            lastname,
+                            username,
+                            email,
+                            dep_id
+                        },
+                    }, process.env.ACCESS_TOKEN_SECRET,
+                    {
+                        expiresIn: "20m"
+                    });
+                    res.status(200).json({ access_token });
+                } else {
+                    console.log("Invalid password");
+                    res.status(200).json({
+                        error: "Invalid password"
+                    });
+                }
+            });
         }
     })
 }
 
+//END OF NEW LOGIN 
 const register = async (req, res) => {
     const {firstname, lastname, username, email, password, department} = await req.body;
     const hashed_password = await bcrypt.hash(password, 8)
